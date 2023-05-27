3c4c10e8710ed8a52b451aa6e3ea35936820237a
Lívia de Oliveira Táparo <125320957+LiviaTaparo@users.noreply.github.com>
2023-05-26 21:55:24 -0300

    Add files via upload

---
+++ Desafio_Fila_Banco_26-05/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+struct
+{
+    string cliente;
+    double saldo;
+
+}Conta;
+
+int main()
+{
+   queue <Conta> atendimento;
+   int a=0;
+
+   do
+   {
+        cout << "\nEscolha um numero:\n1.Abrir uma conta\n2.Atender Proximo Cliente\n0.Encerrrar Programa\nEscolha um numero: \n";
+        cin >> a;
+
+        switch(a)
+        {
+        case 1:
+
+        }
+
+   }
+}
